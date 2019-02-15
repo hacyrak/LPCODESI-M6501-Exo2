a1d65f1ae94578bad4e227af348da7c0009fb7e1
A1007744 <A1007744@a-f004243.salsa.univ-amu.fr>
2019-02-15 14:49:58 +0100

    création de la classe CartePaiement

---
+++ LPCODESI-M6501-Exo2/src/CartePaiement.java
@@ -0,0 +1,13 @@
+
+public abstract class CartePaiement {
+	
+	protected CartePaiement() {
+		
+	}
+	
+	public void payeAchat(Double montant) {
+		
+	}
+	
+	public abstract void autorisePaiement(Double montant);
+}
