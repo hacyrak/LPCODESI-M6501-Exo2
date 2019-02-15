bcd48389806ac6658d9f2175d344792566a43706
A1007744 <A1007744@a-f004243.salsa.univ-amu.fr>
2019-02-15 14:56:53 +0100

    creation de la classe Compte

---
+++ LPCODESI-M6501-Exo2/src/Compte.java
@@ -0,0 +1,13 @@
+
+public class Compte {
+	private Double solde;
+	public Compte() {
+		
+	}
+	public Double getSolde() {
+		return solde;
+	}
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
+}
